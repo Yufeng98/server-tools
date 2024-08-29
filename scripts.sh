e302c0943c61bd5049cb7849b0ab31e296250b98
Yufeng98 <yufenggu@umich.edu>
2024-08-29 16:46:30 -0400

    Update users

--- scripts.sh
+++ scripts.sh
@@ -1,3 +1,6 @@
-bash add_sudo_user.sh kaplannp
-bash add_sudo_user.sh sumanthu
-bash add_sudo_user.sh arkhadem
+#bash add_sudo_user.sh kaplannp
+#bash add_sudo_user.sh sumanthu
+#bash add_sudo_user.sh arkhadem
+
+bash add_sudo_user.sh cruskin 
+bash add_sudo_user.sh akashpt 
